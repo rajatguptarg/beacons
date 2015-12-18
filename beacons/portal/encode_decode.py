bd289f74bf4841ee49bfecea20744180f8fce5ca
parulsha <parulsha@INparulsha.local>
2015-12-18 11:26:28 +0530

    Portal takes in attachment to beacons in form of json

--- beacons/portal/encode_decode.py
+++
@@ -1,4 +0,0 @@
-import base64
-
-a=base64.b64encode("hello world")
-print a
--- beacons/portal/view/views.py
+++ beacons/portal/view/views.py
@@ -1,12 +1,12 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, flash, render_template, request
+from flask.ext.session import Session
 import flask
 import requests
 from oauth2client import client
 import json
-from config import LIST_BEACONS, SCOPE
+from config import LIST_BEACONS, SCOPE, ATTACHMENT
 from beacons.portal.controller import controller
 from beacons.portal.models import Beacon, Header
-
 
 portal = Blueprint('portal', __name__)
 session = requests.Session()
@@ -19,6 +19,7 @@ def list_beacons():
     credentials = client.OAuth2Credentials.from_json(
         flask.session['credentials']
     )
+
     if credentials.access_token_expired:
         return flask.redirect(flask.url_for('portal.oauth2callback'))
     else:
@@ -48,10 +49,14 @@ def oauth2callback():
         flask.session['credentials'] = credentials.to_json()
         return flask.redirect(flask.url_for('portal.list_beacons'))
 
+@portal.route('/attachment')
+def attachment_beacons():
+    return render_template('attachment.jinja')
 
 @portal.route('/register', methods=['GET'])
 def register_beacons():
     return render_template('register.jinja')
+
 
 
 @portal.route('/register', methods=['POST'])
@@ -83,6 +88,7 @@ def unregister_beacons_status():
     credentials = client.OAuth2Credentials.from_json(
         flask.session['credentials']
     )
+
     if credentials.access_token_expired:
         return flask.redirect(flask.url_for('portal.oauth2callback'))
     else:
@@ -131,11 +137,12 @@ def beacon_namespace():
         return flask.redirect(flask.url_for('portal.oauth2callback'))
     else:
         header = Header(credentials.access_token)
-        response = requests.get(config.ATTACHMENT, headers=header.__str__())
-        status = \
-            config.ERROR if response.status_code is 400 else config.SUCCESS
-	
-        return render_template('namespace_status.jinja', status=json.loads(response.content))
+        response = requests.get(ATTACHMENT,
+            headers=header.__str__())
+
+        return render_template(
+            'namespace_status.jinja', status=json.loads(response.content)
+        )
 
 
 @portal.route('/attachment-status', methods=['POST'])
@@ -143,24 +150,28 @@ def beacon_attachment_status():
     if credentials.access_token_expired:
         return flask.redirect(flask.url_for('portal.oauth2callback'))
     else:
-        form = BeaconName(request.form)
-        if request.method == 'POST' and form.validate():
-            request_body = Beacon.attachment_request_body(form)
-            header = Header(credentials.access_token)
-	        url = config.ATTACH_BEACONS + beacon.name + config.ATTACH
-            response = requests.post(
-                url, data=json.dumps(request_body),
-                headers=header.__str__()
+        beacon = Beacon(request.form.get('advid'))
+        beacon.name = request.form.get('name')
+        beacon.msg = request.form.get('msg')
+
+        try:
+            json_object = json.loads(beacon.msg)
+        except ValueError, e:
+            flash('Invalid Input')
+
+        request_body = beacon.attachment_request_body()
+        header = Header(credentials.access_token)
+        url = config.ATTACH_BEACONS + beacon.name + config.ATTACH
+        response = requests.post(
+            url, data=(json.dumps(request_body)),
+            headers=header.__str__()
             )
-
-	        status = json.loads(response.content)
-
-            for key, value in status.iteritems():
-		      if(key == 'data'):
-                  ans = base64.b64decode(status[key])
-	    			
-            return render_template(
-		      'attachment_status.jinja',status = ans
- 		    )
-
-        return render_template('attachment.jinja', form=form)
+        status1 = json.loads(response.content)
+        for key, value in status1.iteritems():
+            if(key == 'data'):
+                ans = base64.b64decode(status1[key])
+                finalans = json.loads(ans)
+        return render_template(
+            'attachment_status.jinja', status1=finalans,
+            status=json.loads(response.content)
+            )
