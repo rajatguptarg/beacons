32fb8bf70a9a55cf09f71ec689a400370ddc2831
Rajat Gupta <rajat.gupta712@gmail.com>
2015-12-29 14:49:34 +0530

    Added 405 Error Handling

--- beacons/portal/view/error_handler.py
+++ beacons/portal/view/error_handler.py
@@ -11,7 +11,7 @@ def page_not_found(e):
     """
     Handle 404 Errors
     """
-    beacons.app.logger.error('404 Occured. Explanation: ' + e)
+    beacons.app.logger.error('404 Occured. Explanation: ' + str(e))
     return render_template('404.jinja'), 404
 
 
@@ -20,7 +20,7 @@ def forbidden(e):
     """
     Handle 403 Errors
     """
-    beacons.app.logger.error('403 Occured. Explanation: ' + e)
+    beacons.app.logger.error('403 Occured. Explanation: ' + str(e))
     return render_template('403.jinja'), 403
 
 
@@ -29,8 +29,17 @@ def gone(e):
     """
     Handle 410 Errors
     """
-    beacons.app.logger.error('410 Occured. Explanation: ' + e)
+    beacons.app.logger.error('410 Occured. Explanation: ' + str(e))
     return render_template('410.jinja'), 410
+
+
+@portal.app_errorhandler(405)
+def method_not_allowed(e):
+    """
+    Handle 405 Errors
+    """
+    beacons.app.logger.error('405 Occured. Explanation: ' + str(e))
+    return render_template('405.jinja'), 405
 
 
 @portal.app_errorhandler(500)
@@ -38,7 +47,7 @@ def internal_server_error(e):
     """
     Handle 500 Errors
     """
-    beacons.app.logger.error('500 Occured. Explanation: ' + e)
+    beacons.app.logger.error('500 Occured. Explanation: ' + str(e))
     return render_template('500.jinja'), 500
 
 
@@ -47,5 +56,5 @@ def exceptions(e):
     """
     Handle All Exceptions
     """
-    beacons.app.logger.error('Exception Occured. Explanation: ' + e)
+    beacons.app.logger.error('Exception Occured. Explanation: ' + str(e))
     return render_template('exception.jinja', exception=e)
