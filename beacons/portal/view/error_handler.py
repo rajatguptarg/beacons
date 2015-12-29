a1bcd3566d1fe6da71e2a94e80ad28cba7f05348
Rajat Gupta <rajat.gupta712@gmail.com>
2015-12-29 13:07:32 +0530

    Refactored Error Handling

--- beacons/portal/view/error_handler.py
+++ beacons/portal/view/error_handler.py
@@ -1,27 +1,46 @@
+"""
+Author: Rajat Gupta
+"""
+
 from flask import render_template
 from beacons.portal.view import portal
 
 
 @portal.app_errorhandler(404)
 def page_not_found(e):
+    """
+    Handle 404 Errors
+    """
     return render_template('404.jinja'), 404
 
 
 @portal.app_errorhandler(403)
 def forbidden(e):
+    """
+    Handle 403 Errors
+    """
     return render_template('403.jinja'), 403
 
 
-@portal.errorhandler(410)
+@portal.app_errorhandler(410)
 def gone(e):
+    """
+    Handle 410 Errors
+    """
     return render_template('410.jinja'), 410
 
 
 @portal.app_errorhandler(500)
 def internal_server_error(e):
+    """
+    Handle 500 Errors
+    """
     return render_template('500.jinja'), 500
 
 
-@portal.errorhandler(Exception)
+@portal.app_errorhandler(Exception)
 def exceptions(e):
+    """
+    Handle All Exceptions
+    """
     return render_template('exception.jinja', exception=e)
